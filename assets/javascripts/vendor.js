0d23b653bdd4a371060de61af8d3c1a80a8d720a
Adam Moon <itchcork@gmail.com>
2013-09-28 21:59:27 -0500

    Update vendor.js

--- assets/javascripts/vendor.js
+++ assets/javascripts/vendor.js
@@ -9304,48 +9304,8 @@ R).push([b,c,d])};define.amd={jQuery:!0};j.exec=function(b){return eval(b)};j(t)
 require.config({
     baseUrl: "https://raw.github.com/adamjmoon/ToDoApp/master/assets/javascripts/",
     paths: {
-        'text': 'vendor/requirejs-text/text',
-        'plugins': 'durandal/plugins',
-        'transitions': 'durandal/transitions',
         'app': 'app'
     }
 });
-require(['app'], function () {
-        "use strict";
-
-        define('knockout', function () {
-            return window.ko
-        });
-        define('jquery', function () {
-            return window.$
-        });
-
-
-        require(['durandal/app', 'durandal/viewLocator', 'durandal/system', 'localStorage', 'OT'], function (app, viewLocator, system, LocalStorage, OT) {
-
-            OT.DataService = new LocalStorage();
-            //>>excludeStart("build", true);
-            system.debug(true);
-            //>>excludeEnd("build");
-
-            app.title = 'Todo Durandal';
-
-
-            app.configurePlugins({
-                router: true
-            });
-
-            app.start().then(function () {
-                //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
-                //Look for partial views in a 'views' folder in the root.
-                viewLocator.useConvention();
-
-                //Show the app by setting the root view model for our application with a transition.
-
-                app.setRoot('viewmodels/shell');
-            });
-        });
-
-    }
-);
+require('app');
 
